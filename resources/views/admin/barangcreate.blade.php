1cbe87cead481bf7c4e56ec86d2f5f32639a0449
alxndr29 <61173884+alxndr29@users.noreply.github.com>
2020-09-16 20:22:58 +0700

    Initial commit

---
+++ resources/views/admin/barangcreate.blade.php
@@ -0,0 +1,28 @@
+@extends('admin.header')
+
+@section('content')
+<!-- /.col (left) -->
+<div class="col mt-3">
+    <div class="card card-primary">
+        <div class="card-header">
+            <h3 class="card-title">Tambah Barang</h3>
+        </div>
+        <div class="card-body">
+            <div class="form-group">
+                <label for="exampleInputEmail1">Email address</label>
+                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+            </div>
+            <div class="form-group">
+                <label for="exampleInputPassword1">Password</label>
+                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+            </div>
+            <button class="btn btn-primary">Tambah</button>
+        </div>
+        <div class="card-footer">
+            Visit <a href="https://tempusdominus.github.io/bootstrap-4/Usage/">tempusdominus </a> for more examples and information about
+            the plugin.
+        </div>
+    </div>
+</div>
+
+@endsection
